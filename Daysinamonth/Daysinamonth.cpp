b15ef15edf8ba70f040b463cc16502a5d1854e40
ErikaH92 <erv27240@email.vccs.edu>
2025-04-24 23:58:00 -0400

    Completed code

--- Daysinamonth/Daysinamonth.cpp
+++ Daysinamonth/Daysinamonth.cpp
@@ -7,7 +7,41 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int month = 0;
+    int year = 0;
+
+    cout << "Enter the number of month (1-12) \n"; 
+    cin >> month; 
+
+    switch (month) {
+
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+
+        cout << "This month has 30 days. \n";
+        break;
+    case 4: case 6: case 9: case 11:
+        cout << "this month has 30 days.\n";
+        break;
+    case 2:
+
+        cout << "Enter the year\n";
+        cin >> year;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+
+            cout << "February in " << year << " has 29 days. (leap year)";
+        
+        } else {
+            cout << " February in " << year << " has 28 days.\n";
+        }
+        break;
+        default:
+        cout << "invalid month. Please enter a number between 1 and 12.\n";
+        break;
+
+    }
+
+        return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
